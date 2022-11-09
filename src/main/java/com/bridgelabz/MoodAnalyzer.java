08c1907aac8e3122cbe600ffdbdba60efa7c9b59
PremKumbhar <kumbharprem7738@gmail.com>
2022-11-09 12:06:08 +0530

    [Prem]:Added refactored the code to take message in constructor

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
-    public String analyseMood(String message) {
+    private String message;
+
+    public MoodAnalyzer() {
+    }
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
         if (message.contains("sad")){
             return "SAD";
         }
--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -6,15 +6,15 @@ import org.junit.Test;
 public class MoodAnalyzerTest {
     @Test
     public void testMoodAnalysis(){
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String result = moodAnalyzer.analyseMood("I am in sad mood");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in sad mood");
+        String result = moodAnalyzer.analyseMood();
         Assert.assertEquals("SAD",result);
 
     }
     @Test
     public void givenHappyMessage_shouldReturnHappy(){
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String result = moodAnalyzer.analyseMood("I am in any mood");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any mood");
+        String result = moodAnalyzer.analyseMood();
         Assert.assertEquals("HAPPY",result);
     }
 }
